c79b3b37e00bcaeaab8feb6b7d689b8e97af9853
OcttaDev <127272659+OcttaDev@users.noreply.github.com>
2023-03-09 19:58:25 -0300

    test

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Nav from "../../Nav/Nav";
+import Nav from "./Nav/Nav";
 import "../../../responsive.css";
 
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './components/Index'
-import Contatos from './components/pages/Contatos'
+import Home from './components/Home'
+import Contatos from './components/Contatos'
 import Locais from './components/pages/Locais'
 import Header from "./components/Header/Header";
 
